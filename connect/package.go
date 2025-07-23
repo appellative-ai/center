1aba031e7bde9b1d620b4058671456954107e4a4
appellative-ai <mark.wilson.ai@outlook.com>
2025-07-23 15:16:02 -0500

    Update package.go

--- connect/package.go
+++
@@ -1,6 +0,0 @@
-package connect
-
-// Namespace - create a connection
-//
-// Path :
-//        POST /namespace/connect
---
+++ relation/package.go
@@ -0,0 +1,8 @@
+package relation
+
+// Namespace -
+//     1. find a related name/thing - allow definition of closeness, like cousins, 2nd cousins
+//     2. validate a given can be related to an aspect - allow definition of closeness
+//
+// Path :
+//        POST /namespace/relation
