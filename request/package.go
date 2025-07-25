bb09c467d8b7b0ebfead1811973c1767fb1f7f1c
appellative-ai <mark.wilson.ai@outlook.com>
2025-07-25 07:08:29 -0500

    Updating

--- request/package.go
+++ request/package.go
@@ -4,3 +4,10 @@ package request
 //
 // Path : POST /namespace/request
 //
+
+// Namespace -
+//     1. find a related name/thing - allow definition of closeness, like cousins, 2nd cousins
+//     2. validate a given can be related to an aspect - allow definition of closeness
+//
+// Path :
+//        POST /namespace/relation
---
+++ resolution/agent_test.go
@@ -0,0 +1,13 @@
+package resolution
+
+import "fmt"
+
+func ExampleNewAgent() {
+	a := newAgent(nil)
+
+	fmt.Printf("test: newAgent() -> [%v]\n", a)
+
+	//Output:
+	//test: newAgent() -> [common:core:agent/namespace/center]
+
+}
