89e88b7303e2ec2a2283fc9cc0cf3bfc4773bed4
appellative-ai <mark.wilson.ai@outlook.com>
2025-08-22 09:29:26 -0500

    Updating

---
+++ operations/registry.go
@@ -0,0 +1,15 @@
+package operations
+
+type Link struct {
+	Collective string
+	Host1      string
+	Host2      string
+}
+
+// Really just a slice of maps
+const (
+	CollectiveName = "collective"
+	Host1Name      = "host1"
+	Host2Name      = "host2"
+	//RegistryAuthorization
+)
---
+++ operations/todo.go
@@ -0,0 +1,8 @@
+package operations
+
+// How to startup and support concurrent requests to linked collectives
+// We obviously need the registry host names
+
+// The registry would contain the following data:
+//   A slice
+//
